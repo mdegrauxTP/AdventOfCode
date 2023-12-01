c8e8d79028f0684a957f5f9382f0fcdc90ee3ff4
mdegrauxTP <89838999+mdegrauxTP@users.noreply.github.com>
2023-12-01 11:03:29 +0100

    clean

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -24,15 +24,13 @@ public class Day01 : BaseDay
     {
         string pattern = @"([0-9]|one|two|three|four|five|six|seven|eight|nine)";
         string patternBack = @"([0-9]|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)";
-        //var matches = _input.Select(l => Regex.Matches(l, pattern).Cast<Match>().Select(x => Convert(x.Value)));
-//        var result = matches.Select(m => int.Parse(m.First().ToString() + m.Last().ToString())).Sum();
 
         int count = 0;
         foreach(var line in _input)
         {
             var first = Regex.Match(line, pattern);
             var last = Regex.Match(new string(line.Reverse().ToArray()), patternBack);
-            count += int.Parse(Convert(first.ToString()) + ConvertBackward(last.ToString()));
+            count += int.Parse(Convert(first.ToString()) + Convert(last.ToString()));
         }
         return new(count.ToString());
     }
@@ -41,33 +39,16 @@ public class Day01 : BaseDay
     {
         return digitAsString switch
         {
-            "one" => "1",
-            "two" => "2",
-            "three" => "3",
-            "four" => "4",
-            "five" => "5",
-            "six" => "6",
-            "seven" => "7",
-            "eight" => "8",
-            "nine" => "9",
+            "one" or "eno" => "1",
+            "two" or "owt" => "2",
+            "three" or "eerht" => "3",
+            "four" or "ruof" => "4",
+            "five" or "evif" => "5",
+            "six" or "xis" => "6",
+            "seven" or "neves" => "7",
+            "eight" or "thgie" => "8",
+            "nine" or "enin" => "9",
             _ => digitAsString
         };
     }
-
-    private string ConvertBackward(string digitAsString)
-    {
-        return digitAsString switch
-        {
-            "eno" => "1",
-            "owt" => "2",
-            "eerht" => "3",
-            "ruof" => "4",
-            "evif" => "5",
-            "xis" => "6",
-            "neves" => "7",
-            "thgie" => "8",
-            "enin" => "9",
-            _ => digitAsString
-        };
-    }
-}
+ }
